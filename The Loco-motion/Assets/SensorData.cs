0b9617fc25aa8aebc94b5fd78e3b86eb2dce433f
Sofia Wyetzner <sofia.wyetzner@gmail.com>
2017-02-17 16:38:26 -0600

    initialized repository

---
+++ The Loco-motion/Assets/SensorData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SensorData : NetworkBehaviour {
+	private Vector3 pos;
+	private Quaternion rot;
+
+	void Update () {
+		if (!isLocalPlayer)
+			return;
+		pos = Input.acceleration;
+		rot = Input.gyro.attitude;
+	}	
+}
