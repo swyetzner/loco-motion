b3233e3fa3a920a2fdda193f794c30f50a3ecbd3
Sofia Wyetzner <sofia.wyetzner@gmail.com>
2017-03-02 23:08:59 -0600

    Added networking scripts

---
+++ The Loco-motion/Assets/FootScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DataType {
+	public static short Data = MsgType.Highest + 1;
+};
+
+public class DataMessage : MessageBase {
+	public float pos;
+	public string comment;
+}
+
+public class FootScript : MonoBehaviour {
+	NetworkClient client;
+	public const short RegisterHostMsgId = 888;
+
+	void ConnectToServer() {
+		NetworkConnectionError error = Network.Connect ("127.0.0.1", 25000, "bears");
+		Debug.Log("Could not connect to server: " + error);
+	}
+
+	public void OnConnected(NetworkMessage netMsg) {
+		Debug.Log("Connected to server");
+	}
+
+	void OnConnectedToServer() {
+		Debug.Log ("Connected to server");
+	}
+	void OnFailedToConnect(NetworkConnectionError error) {
+		Debug.Log("Could not connect to server: " + error);
+	}
+
+	public void SendDataMessage() {
+		DataMessage msg = new DataMessage ();
+		msg.comment = "test";
+		msg.pos = 5.0f;
+
+		client.Send (DataType.Data, msg);
+	}
+
+
+	void Start() {
+		client = new NetworkClient ();
+		client.RegisterHandler (MsgType.Connect, OnConnected);
+		client.Connect ("127.0.0.1", 25000);
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.C)){
+			client.Connect ("127.0.0.1", 25000);
+			SendDataMessage ();
+			if (client.isConnected) {
+				SendDataMessage ();
+			} else {
+				Debug.Log ("not connected");
+			}
+		}
+	}
+}
---
+++ The Loco-motion/Assets/HeadScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HeadScript : MonoBehaviour {
+	private int connectCount = 1;
+	NetworkServerSimple server;
+
+	void LaunchServer() {
+		Network.incomingPassword = "bears";
+		bool useNat = !Network.HavePublicAddress ();
+		Network.InitializeServer (2, 25000, useNat);
+	}
+	void OnServerInitialized() {
+		Debug.Log("Server initialized and ready");
+	}
+	void OnPlayerConnected(NetworkPlayer player) {
+		Debug.Log ("Device " + connectCount++ + " connected from  " + player.ipAddress + ":" + player.port);
+	}
+
+	void OnConnected(NetworkMessage netMsg) {
+		Debug.Log ("Server connected");
+	}
+		
+	public void OnPos(NetworkMessage netMsg) {
+		DataMessage msg = netMsg.ReadMessage<DataMessage> ();
+		Debug.Log ("OnPos " + msg.comment + " " + msg.pos);
+	}
+
+	void Start() {
+		server = new NetworkServerSimple ();
+		server.RegisterHandler (MsgType.Connect, OnConnected);
+		server.RegisterHandler (DataType.Data, OnPos);
+		server.Initialize ();
+		ConnectionConfig config = new ConnectionConfig ();
+		config.AddChannel(QosType.Reliable);
+		server.Configure (config, 2);
+		server.Listen (25000);
+		Debug.Log("Server Listening on " + server.listenPort);
+	}
+
+	void Update() {
+	}
+		
+}
