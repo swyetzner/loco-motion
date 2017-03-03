7d85a396c0c98866bb8fba0db2ff29b7f4df84c5
Sofia Wyetzner <sofia.wyetzner@gmail.com>
2017-03-03 15:32:41 -0600

    Real script thing

---
+++ The Loco-motion/Assets/ClientScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ClientScript : NetworkBehaviour {
+	public Text PositionOutput;
+	public Text RotationOutput;
+
+	[Command]
+	void CmdSendPosition(Vector3 pos, Quaternion rot) {
+		Debug.Log ("Sending " + pos + " " + rot);
+		//PositionOutput.text = pos.ToString ();
+		//RotationOutput.text = rot.ToString ();
+	}
+
+
+	void Update () {
+		if (isClient) {
+			Vector3 pos = Input.acceleration;
+			Quaternion rot = Input.gyro.attitude;
+			CmdSendPosition (pos, rot);
+		}
+	}
+}
